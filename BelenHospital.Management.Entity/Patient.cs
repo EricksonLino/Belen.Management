fee005601a66ea940f31923de4b1da25c614a4ac
Erickson L <linobarrantes@gmail.com>
2023-03-01 22:53:23 -0500

    Add project files.

---
+++ BelenHospital.Management.Entity/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BelenHospital.Management.Entity
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(8)]
+        public int Dni { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string StoryNumber { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public DateTime Birthdate { get; set; }
+        [Required]
+        public string InsuranceCompany { get; set; }
+        [Required]
+        public string Nationality { get; set; }
+    }
+}
---
+++ BelenHospital.Management.Entity/Specilty.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BelenHospital.Management.Entity
+{
+    public class Specilty
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(12)]
+        public int NumberOfDoctors { get; set; }
+        [Required]
+        public bool IsEnabled { get; set; }
+    }
+}
